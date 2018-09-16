905a625d9af739cdd1dfa1dbd7f77d7639d8d2e4
Patrick Carr <pwcsquared@gmail.com>
2018-09-15 20:34:18 -0500

    refactored

--- game-of-life.js
+++ game-of-life.js
@@ -1,9 +1,20 @@
 'use strict';
 
-const length = 100;
+const length = 500;
 let previousBoardState = [];
+let gameboard = createGameBoard(length);
+let intervalID;
+const canvasConfig = {
+    fillColor: 'dodgerblue',
+    size: 1,
+}
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+ctx.fillStyle = canvasConfig.fillColor;
+let neighbors = generateNeighbors();
+console.log('neighbors', neighbors);
 
-const createGameBoard = function(dimension) {
+function createGameBoard(dimension) {
     var board = [];
     for (let i = 0; i < dimension; i++) {
         board.push([]);
@@ -15,17 +26,15 @@ const createGameBoard = function(dimension) {
     return board;
 };
 
-const wrapIndex = function(val) {
-    if (val >= length) {
-        return 0;
-    } else if (val < 0) {
-        return length - 1;
-    } else {
-        return val;
-    }
+function generateNeighbors() {
+    return gameboard.map(function(row, i) {
+        return row.map(function(_cell, j) {
+            return getNeighbors(i,j);
+        })
+    })
 };
 
-const getNeighbors = function(col, row) {
+function getNeighbors(col, row) {
     var neighbors = [];
     for (let i = col - 1; i <= col + 1; i++) {
         for (let j = row - 1; j <= row + 1; j++) {
@@ -38,53 +47,54 @@ const getNeighbors = function(col, row) {
     return neighbors;
 };
 
-const getLifeCount = function(neighbors) {
+function wrapIndex(val) {
+    if (val >= length) {
+        return 0;
+    } else if (val < 0) {
+        return length - 1;
+    } else {
+        return val;
+    }
+};
+
+function getLifeCount(neighbors) {
     // neighbors are a pair of coordinates [col, row]
     return neighbors.reduce((acc, val) => acc + gameboard[val[0]][val[1]], 0);
 };
 
-const willLive = function(isAlive, lifeCount) {
+function willLive(isAlive, lifeCount) {
     if (isAlive) {
         return (lifeCount === 2 || lifeCount === 3);
     }
     return (lifeCount === 3);
 };
 
-const updateGameboard = function() {
+function updateGameboard() {
     previousBoardState = Array.from(gameboard);
     gameboard = gameboard.map(function(row, i) {
         return row.map(function(cell, j) {
-            var neighbors = getNeighbors(i, j);
-            var lifeCount = getLifeCount(neighbors);
+            let lifeCount = getLifeCount(neighbors[i][j]);
             return Number(willLive(cell, lifeCount));
         });
     });
 };
 
-const canvasConfig = {
-    fillColor: 'dodgerblue',
-    size: 5,
-}
-
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
-ctx.fillStyle = canvasConfig.fillColor;
-
-var writeout = function(boardState, {size}){
+function writeout(boardState, {size}){
     boardState.forEach(function(item, index) {
         item.forEach(function(_cell, deeperIndex) {
             let xPos = index * size;
             let yPos = deeperIndex * size;
-            if (boardState[index][deeperIndex] === 1) {
-                ctx.fillRect(xPos, yPos, size, size);  // for live cells
-            } else {
-                ctx.clearRect(xPos, yPos, size, size); // for dead cells            
+            if (boardState[index][deeperIndex] !== previousBoardState[index][deeperIndex]){
+                if (boardState[index][deeperIndex] === 1) {
+                    ctx.fillRect(xPos, yPos, size, size);  // for live cells
+                } else {
+                    ctx.clearRect(xPos, yPos, size, size); // for dead cells            
+                }
             }
+            // ctx.strokeRect(xPos, yPos, size, size);
         });
     });
 };
-
-let gameboard = createGameBoard(length);
 
 function* updateGame () {
     while(true) {
@@ -93,12 +103,17 @@ function* updateGame () {
     }
 }
 
-let intervalID;
+function runGame(){
+    updateGameboard();
+    writeout(gameboard, canvasConfig);
 
-function play(shouldPlay) {    
-    if (shouldPlay) {
-        intervalID = window.setInterval(() => updateGame().next(), 1000);
+    intervalID = window.requestAnimationFrame(runGame);
+};
+
+function play(doIt = false) {
+    if (doIt) {
+        runGame();
     } else {
-        window.clearInterval(intervalID);
+        window.cancelAnimationFrame(intervalID);
     }
 }
